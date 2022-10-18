995dcc06c7fa46cfa77ed642b4494cd02f2229b7
victoirebeaufils <victoire.beaufil@gmail.com>
2022-10-18 09:44:20 -0400

    Stim channel successfully implemented

--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -1,35 +1,50 @@
 import React, { useRef, useState, useEffect } from "react";
 
 import vid from "./video.mp4";
+import { CSVLink, CSVDownload } from "react-csv";
 
 const Video = (props) => {
-  const [displayPlay, setDisplayPlay] = useState(true);
+  const [displayPlay, setDisplayPlay] = useState(null);
   const [trialsLeft, setTrialsLeft] = useState(null);
   const [timeStamps, setTimeStamps] = useState([]);
   const [arr, setArr] = useState([]); //array of random "pause and play instructions"
   var spacebar = 32; //javascript space bar keycode
+  const [input, setInput] = useState([]);
+  const [play, setPlay] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [currentVideoState, setCurrentVideoState] = useState("pause");
+  var currentIndex = 0;
+  useEffect(() => {
+    var recordingParameters = JSON.parse(
+      localStorage.getItem("recordingParameters")
+    );
+    console.log(recordingParameters);
+    setTrialsLeft(recordingParameters.trials);
+    console.log("game has been initialized");
+  }, []);
 
   useEffect(() => {
     var recordingParameters = JSON.parse(
       localStorage.getItem("recordingParameters")
     );
-    setTrialsLeft(recordingParameters.trials);
-    initializeGame();
-  }, []);
+    if (trialsLeft == recordingParameters.trials) {
+      initializeGame();
+    }
+  }, [trialsLeft]);
 
+  //code taken from Game.js
   function initializeGame() {
+    console.log("in initializeGame function");
+    console.log(trialsLeft);
     for (var i = 0; i < trialsLeft; i++) {
       var n = Math.random() * 10;
       if (n > 5) {
         setArr((arr) => [...arr, "play"]);
-
-        // arr.push("left");
       } else {
         setArr((arr) => [...arr, "pause"]);
-
-        // arr.push("right");
       }
     }
+    console.log(arr);
   }
 
   useEffect(() => {
@@ -41,26 +56,71 @@ const Video = (props) => {
   }, []);
 
   const videoRef = useRef();
+
+  useEffect(() => {
+    console.log("currentVideoState: " + currentVideoState);
+    //At some point, setting the video state to arr[currentIndex] just stops updating the variable
+  }, [currentVideoState]);
+
   const handlePlay = () => {
-    console.log("play");
+    setCurrentVideoState(arr[index]);
+    setIndex(index + 1);
+    setPlay(true);
+    setInput((input) => [...input, "play"]);
+    var time = Date.now();
+    setTimeStamps((timeStamps) => [...timeStamps, time]);
     setTrialsLeft(trialsLeft - 1);
     videoRef.current.play();
   };
   const handlePause = () => {
+    setCurrentVideoState(arr[index]);
+    setIndex(index + 1);
+    setPlay(false);
     setTrialsLeft(trialsLeft - 1);
+    var time = Date.now();
+    setInput((input) => [...input, "pause"]);
+    setTimeStamps((timeStamps) => [...timeStamps, time]);
     videoRef.current.pause();
     console.log("pause");
   };
+  const csvData = [];
+  input.forEach((item, i) => {
+    var obj = { keypress: item, timestamp: timeStamps[i] };
+    csvData.push(obj);
+  });
+  var headers = [
+    { label: "Keypress", key: "keypress" },
+    { label: "Timestamp", key: "timestamp" },
+  ];
+  console.log(csvData);
 
   return (
     <>
       <p>Trials left: {trialsLeft}</p>
 
-      <video width="560" height="315" ref={videoRef}>
-        <source src={vid} type="video/mp4"></source>
-      </video>
-      <button onClick={handlePlay}> Play </button>
-      <button onClick={handlePause}> Pause </button>
+      {trialsLeft == 0 && (
+        <button>
+          <CSVLink
+            data={csvData}
+            headers={headers}
+            filename={"stim-video.csv"} // change file name
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Download Stim channel CSV Data
+          </CSVLink>
+        </button>
+      )}
+      {trialsLeft > 0 && (
+        <>
+          {/* Current video state is not working */}
+          <p>Press on: {currentVideoState}</p>
+          <video width="560" height="315" ref={videoRef}>
+            <source src={vid} type="video/mp4"></source>
+          </video>
+          <button onClick={handlePlay}> Play </button>
+          <button onClick={handlePause}> Pause </button>
+        </>
+      )}
     </>
   );
 };
